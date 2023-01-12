7df7275aa20fc6a63d5c15acdb321aeb4b616583
Настя Карпюк <nastia.karpyuk@outlook.com>
2023-01-12 12:59:50 +0200

    Added DTOs

---
+++ USTest.Common/Models/DTOs/CharacterDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace USTest.Common.Models.DTOs;
+
+public class CharacterDTO
+{
+    public int Id { get; set; }
+    public int Name { get; set; }
+    public string Status { get; set; } = String.Empty;
+    public string Species { get; set; } = String.Empty;
+    public string Type { get; set; } = String.Empty;
+    public string Gender { get; set; } = String.Empty;
+    public OriginDTO Origin { get; set; } = null!;
+}
---
+++ USTest.Common/Models/DTOs/OriginDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace USTest.Common.Models.DTOs;
+
+public class OriginDTO
+{
+    public string Name { get; set; } = String.Empty;
+    public string Type { get; set; } = String.Empty;
+    public string Dimension { get; set; } = String.Empty;
+}
