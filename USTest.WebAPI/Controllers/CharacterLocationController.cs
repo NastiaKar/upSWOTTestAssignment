18e56e61cdf429575932cdf493ec38813310faa7
Настя Карпюк <nastia.karpyuk@outlook.com>
2023-01-15 00:09:25 +0200

    Added main controller

---
+++ USTest.WebAPI/Controllers/CharacterLocationController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using USTest.BLL.Services.Interfaces;
+
+namespace USTest.WebAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CharacterLocationController : ControllerBase
+{
+    private readonly ICharacterService _charService;
+    private readonly ILocationService _locationService;
+
+    public CharacterLocationController(ICharacterService charService, ILocationService locationService)
+    {
+        _charService = charService;
+        _locationService = locationService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetCharacter(string name)
+    {
+        return Ok(await _charService.GetCharacter(name));
+    }
+}
