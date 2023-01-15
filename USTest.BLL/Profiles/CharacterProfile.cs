c3ecb22afeed7cc219cc573b83cec90b580e09df
Настя Карпюк <nastia.karpyuk@outlook.com>
2023-01-15 12:56:31 +0200

    modified CharacterProfile (changed OriginProfile to LocationProfile), added LocationProfile

--- USTest.BLL/Profiles/CharacterProfile.cs
+++ USTest.BLL/Profiles/CharacterProfile.cs
@@ -9,10 +9,10 @@ public class CharacterProfile : Profile
     public CharacterProfile()
     {
         var mapper = new Mapper(new MapperConfiguration(config =>
-            config.AddProfile(new OriginProfile())));
+            config.AddProfile(new LocationProfile())));
 
         CreateMap<Character, CharacterDTO>()
             .ForMember(dest => dest.Origin,
-            opt => opt.MapFrom(src => mapper.Map<OriginDTO>(src.Origin)));
+            opt => opt.MapFrom(src => mapper.Map<LocationDTO>(src.Location)));
     }
 }
---
+++ USTest.BLL/Profiles/LocationProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using USTest.Common.Models.Domain;
+using USTest.Common.Models.DTOs;
+
+namespace USTest.BLL.Profiles;
+
+public class LocationProfile : Profile
+{
+    public LocationProfile()
+    {
+        CreateMap<Location, LocationDTO>();
+    }
+}
