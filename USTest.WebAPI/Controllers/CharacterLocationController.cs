5c776affad960a6388f9f01781bb9357928868fc
Настя Карпюк <nastia.karpyuk@outlook.com>
2023-01-15 13:01:36 +0200

    Controller now returns full model

--- USTest.WebAPI/Controllers/CharacterLocationController.cs
+++ USTest.WebAPI/Controllers/CharacterLocationController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using USTest.BLL.Extensions;
 using USTest.BLL.Services.Interfaces;
+using USTest.Common.Models.DTOs;
 
 namespace USTest.WebAPI.Controllers;
 
@@ -9,16 +12,28 @@ public class CharacterLocationController : ControllerBase
 {
     private readonly ICharacterService _charService;
     private readonly ILocationService _locationService;
+    private readonly IMapper _mapper;
 
-    public CharacterLocationController(ICharacterService charService, ILocationService locationService)
+    public CharacterLocationController(ICharacterService charService, ILocationService locationService, IMapper mapper)
     {
         _charService = charService;
         _locationService = locationService;
+        _mapper = mapper;
     }
 
     [HttpGet]
     public async Task<IActionResult> GetCharacter(string name)
     {
-        return Ok(await _charService.GetCharacter(name));
+        //return Ok(await _charService.GetCharacter("Morty"));
+        var characters = await _charService.GetCharacter(name);
+
+        foreach (var character in characters)
+        {
+            var locationId = StringExtensions.GetLocationId(character.Location.Url);
+            var location = await _locationService.GetLocation(locationId);
+            character.Location = location;
+        }
+
+        return Ok(_mapper.Map<IEnumerable<CharacterDTO>>(characters));
     }
 }
