3aad3ea023d96e97817846459ad439ccef31f53b
Настя Карпюк <nastia.karpyuk@outlook.com>
2023-01-15 13:01:16 +0200

    Added string extensions

---
+++ USTest.BLL/Extensions/StringExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace USTest.BLL.Extensions;
+
+public static class StringExtensions
+{
+    public static int GetLocationId(string url)
+    {
+        Regex regex = new Regex(@"\d*$");
+        int id = Convert.ToInt32(regex.Match(url).Value);
+        return id;
+    }
+}
